92f53d82b58f66fcba1a8e1bf0ca73d3aefa7366
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2021-01-12 21:56:21 +0700

    [admin] เพิ่มหน้าจัดการผู้ใช้งานและหน้าจัดการภาพยนตร์

--- app/Modules/Admin/Config/Routes.php
+++ app/Modules/Admin/Config/Routes.php
@@ -1,3 +1,7 @@
 <?php
-  $routes->get('/admin', '\Modules\Admin\Controllers\Admin::index');
+  $routes->group('admin', ['namespace' => '\Modules\Admin\Controllers'], function($routes) {
+    $routes->get('/', 'Admin::index');
+    $routes->get('user_management', 'Admin::user_management');
+    $routes->get('movie_management', 'Admin::movie_management');
+  });
 ?>
--- app/Modules/Admin/Controllers/Admin.php
+++ app/Modules/Admin/Controllers/Admin.php
@@ -6,5 +6,22 @@
     public function index() {
       echo "Admin";
     }
+
+    public function user_management() {
+      return view('\Modules\Admin\Views\user_management');
+    }
+
+    public function movie_management() {
+      $mode = $this->request->getGet('mode');
+      if (strcmp($mode, 'create') == 0) {
+        return view('\Modules\Admin\Views\movie_management');
+      } elseif (strcmp($mode, 'update') == 0) {
+        $movie_id = $this->request->getGet('movie_id');
+        if (!empty($movie_id) && is_numeric($movie_id)) {
+          $data['movie_id'] = $movie_id;
+          return view('\Modules\Admin\Views\movie_management', $data);
+        } else { return redirect()->route('admin'); }
+      } else { return redirect()->route('admin'); }
+    }
   }
 ?>
